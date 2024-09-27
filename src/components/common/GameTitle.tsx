8508283473bbb73ad6007af640b8675a9ea07106
WYL <wyl@WYLs-MacBook-Air.local>
2024-09-27 11:43:25 -0700

    fixed-text-size

--- src/components/common/GameTitle.tsx
+++ src/components/common/GameTitle.tsx
@@ -1,6 +1,6 @@
 const GameTitle = () => {
     return (
-        <h1 className="sriracha-regular xs:text-[3.5rem] text-[5rem] text-white   text-center">
+        <h1 className="sriracha-regular text-[3.5rem] sm:text-[5rem] text-white   text-center">
             <span className="text-green-300">Tic</span>{" "}
             <span className="text-yellow-300">Tac</span>{" "}
             <span className="text-red-500">Toe</span>
